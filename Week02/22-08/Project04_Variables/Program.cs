8c89cbef4715f60b25029959861776e0dfea6e8b
Mehmet Furkan Demir <furkandemirmehmet@gmail.com>
2024-09-21 14:35:17 +0300

    update

--- Week02/22-08/Project04_Variables/Program.cs
+++ Week02/22-08/Project04_Variables/Program.cs
@@ -4,11 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
+        #region intro
         // int age;
         // age = 30;
-        // System.Console.WriteLine(age);
+        // System.Console.WriteLine(age); 
+        // int age = 49;
+        // System.Console.WriteLine(age);           
+        #endregion
 
-        int age = 49;
-        System.Console.WriteLine(age);
+        #region tam sayılar(integers)
+
+        // byte studentPoint = 63; //8 bit yer kaplar
+        // sbyte number1 = 2;
+        // short number2 = 3;
+        // ushort number3 = 4;
+        // int number4 = 5;
+        // System.Console.WriteLine(studentPoint);
+
+
+
+        #endregion
+
+        #region ondalıklı sayılar
+
+        // float number8 = 34.8f;
+        // double number9 = 23;
+        // decimal number10 = 43.4m;
+
+        #endregion
+
+        #region Diğer tipler
+
+        // char char1 = 's';
+        // bool isActive = false;
+        #endregion
+
+        #region Value Types (Değer tipler)
+
+        #region Reference Types (Referans tipler)
+
+        // string firstName = "Furkan";
+        // System.Console.WriteLine(firstName);
+        // object object1 = 123;
+
+
+        #endregion
+
+        #endregion
     }
 }
